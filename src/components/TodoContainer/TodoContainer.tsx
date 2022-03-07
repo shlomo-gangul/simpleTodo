854e567cbe216b7f9550f6aded2b2fbbe49ef2c3
Shlomo Gangul <shlomog@wix.com>
2022-03-07 18:18:40 +0200

    add drag and drop to the list

--- src/components/TodoContainer/TodoContainer.tsx
+++ src/components/TodoContainer/TodoContainer.tsx
@@ -1,26 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import TodoList from '../TodoList/TodoList';
 import AddTodo from '../AddTodo/AddTodo';
 import s from './todo-container.scss';
 import { Item } from '../../interfaces/item';
 import { ADDED, COMPLETED, DELETED } from '../../consts';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+
+const reorder = (list: Item[], startIndex: number, endIndex: number) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
 
 const TodoContainer: React.FC = () => {
   const [items, setItems] = useState<Item[]>([]);
 
-  const addItem = (value: string) => {
-    setItems([
-      ...items,
-      { content: value, status: ADDED, id: Math.random() * 50 },
-    ]);
-  };
+  const addItem = (content: string) =>
+    setItems([...items, { content, status: ADDED, id: Math.random() * 50 }]);
+
   console.log('container', items);
 
   const modifyStatus = (
     items: Item[],
     id: Item['id'],
     status: Item['status']
-  ) => items.filter((item) => (item.id === id ? (item.status = status) : item));
+  ) =>
+    items.map((item) => {
+      if (item.id === id) {
+        item.status = status;
+      }
+      return item;
+    });
 
   const onAdd = (id: number) => setItems(modifyStatus(items, id, ADDED));
 
@@ -29,15 +41,35 @@ const TodoContainer: React.FC = () => {
 
   const onDelete = (id: number) => setItems(modifyStatus(items, id, DELETED));
 
+  const onDragEnd = (result: any) => {
+    if (!result.destination) {
+      return;
+    }
+    const newOrder = reorder(
+      items,
+      result.source.index,
+      result.destination.index
+    );
+    setItems(newOrder);
+    // the only one that is required
+  };
   return (
     <div className={s.todoContanier}>
       <AddTodo addItem={addItem} />
-      <TodoList
-        items={items}
-        onDone={onComplete}
-        onRedo={onAdd}
-        onDelete={onDelete}
-      />
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable-1">
+          {(provided, snapshot) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              <TodoList
+                items={items}
+                onComplete={onComplete}
+                onRedo={onAdd}
+                onDelete={onDelete}
+              />
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -3,20 +3,31 @@ import TodoItem from '../TodoItem/TodoItem';
 import { TodoList as ITodoList } from '../../interfaces/todoList';
 import s from './todo-list.scss';
 import { DELETED } from '../../consts';
+import { Draggable } from 'react-beautiful-dnd';
 
-const TodoList: ITodoList = ({ items, onDone, onRedo, onDelete }) => {
+const TodoList: ITodoList = ({ items, onComplete, onRedo, onDelete }) => {
   return (
     <ul className={s.list}>
       {items.map(
         (item, index) =>
           item.status !== DELETED && (
-            <TodoItem
-              key={index}
-              item={item}
-              onDone={onDone}
-              onRedo={onRedo}
-              onDelete={onDelete}
-            />
+            <Draggable draggableId={item.id.toString()} index={index}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                >
+                  <TodoItem
+                    key={index}
+                    item={item}
+                    onComplete={onComplete}
+                    onRedo={onRedo}
+                    onDelete={onDelete}
+                  />
+                </div>
+              )}
+            </Draggable>
           )
       )}
     </ul>
